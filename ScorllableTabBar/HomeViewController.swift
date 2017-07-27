633a7bb87d0e43ec972afb1ac388d45ce8eae0f0
Mahabaleshwar <mahabaleshwarhnr@gmail.com>
2017-07-27 13:03:53 +0530

    Implements top scroll bar

---
+++ ScorllableTabBar/HomeViewController.swift
@@ -0,0 +1,105 @@
+//
+//  HomeViewController.swift
+//  ScorllableTabBar
+//
+//  Created by Kulkarni on 27/07/17.
+//  Copyright © 2017 Shrilakshmi Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var navBar: UIView!
+    
+    @IBOutlet weak var tabCollectionView: UICollectionView!
+    
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+    
+    var highlightView = UIView()
+    var labelArrays = ["Test1","Test2","Test3","Test 4","Test 5","Test 6"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+        self.setupHighLightView(frame: CGRect(x: 0, y: 30, width: 50, height: 2))
+        self.tabCollectionView.reloadData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+
+extension HomeViewController {
+    
+    func selectItemAt(index : Int) {
+        let indexPath = IndexPath(item: index, section: 0)
+        self.tabCollectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredHorizontally)
+        
+        collectionView(self.tabCollectionView, didSelectItemAt: indexPath)
+        
+    }
+    
+    
+    func setupHighLightView(frame:CGRect) {
+        highlightView.frame = frame
+        highlightView.backgroundColor = UIColor.red
+        self.tabCollectionView.addSubview(highlightView)
+    }
+    
+    func setupCollectionView() {
+        self.tabCollectionView.delegate = self
+        self.tabCollectionView.dataSource = self
+        self.tabCollectionView.register(LabelCollectionViewCell.nib(), forCellWithReuseIdentifier: LabelCollectionViewCell.cellIdentifier())
+        self.tabCollectionView.isPagingEnabled = true
+        self.tabCollectionView.allowsSelection = true
+    }
+    
+}
+
+
+extension HomeViewController : UICollectionViewDelegate,UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LabelCollectionViewCell.cellIdentifier(), for: indexPath) as! LabelCollectionViewCell
+        self.configureCell(cell: cell, indexPath: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return labelArrays.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)!
+        print(cell.frame.origin.y)
+        print(cell.frame.height)
+        UIView.animate(withDuration: 0.3) {
+            self.highlightView.frame.origin.x = cell.frame.origin.x
+        }
+    }
+    
+    func configureCell(cell : LabelCollectionViewCell,indexPath : IndexPath) {
+        let label = self.labelArrays[indexPath.row]
+        cell.labelName.text = label
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ScorllableTabBar/ViewController.swift
+++ ScorllableTabBar/ViewController.swift
@@ -1,25 +1,62 @@
 //
 //  ViewController.swift
-//  ScorllableTabBar
+//  tabBar
 //
-//  Created by Kulkarni on 27/07/17.
+//  Created by Kulkarni on 22/07/17.
 //  Copyright © 2017 Shrilakshmi Kulkarni. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+//TabbarController -> ViewController -> PageViewController (child) -> BaseChildViewControllers(n) -> SpecificChildviewControllers -> Custom UI
 
+class ViewController: UIViewController {
+    
+    var sideScrollViewController : SideScrollViewController!
+    var mainPageViewController : MainPageViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mainPageViewControllerSegue" {
+            let mainPageViewController = segue.destination as! MainPageViewController
+            mainPageViewController.informingDelegate = self
+            self.mainPageViewController = mainPageViewController
+            
+            
+        }
+        if segue.identifier == "sideScrollViewControllerSegue" {
+            let sideScrollViewController = segue.destination as! SideScrollViewController
+            self.sideScrollViewController = sideScrollViewController
+            self.sideScrollViewController.delegate = self
+        }
+    }
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+extension ViewController : MainPageViewControllerDelegate {
+    func mainPageViewController(mainPageViewController: MainPageViewController, didMovePageTo location: Int) {
+        self.sideScrollViewController.selectItemAt(index: location)
+    }
+}
+
+extension ViewController : SideScrollViewControllerDelegate {
+    func sideScrollViewController(sideScrollViewController: SideScrollViewController, didMoveTo location: Int) {
+        self.mainPageViewController.moveTo(page: location)
+    }
+}
+
+
 
